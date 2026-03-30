5b71a9e52d0b3a7e682c079cdaa5a701c6e29ce9
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-03-30 00:45:28 -0400

    Strictly load .lua only on init script
    For consistency

--- src/init.lua
+++ src/init.lua
@@ -9,18 +9,18 @@ if not LightingSystem_loaded then rawset(_G, "LightingSystem_loaded", false) end
 if not LightObjects then rawset(_G, "LightObjects", {}) end --Initialize table for light assignment per object.
 
 if not LightingSystem_loaded then
-	dofile(LightingSystem_root.."Freeslots")
+	dofile(LightingSystem_root.."Freeslots.lua")
 	if isdedicatedserver then LightingSystem_loaded = true return end --don't even handle coronas for the dedicated server
 	
 	--Must load first
-	dofile(LightingSystem_root.."Commands")
-	dofile(LightingSystem_root.."Functions")
-	dofile(LightingSystem_root.."Corona")
-	dofile(LightingSystem_root.."Super player corona")
+	dofile(LightingSystem_root.."Commands.lua")
+	dofile(LightingSystem_root.."Functions.lua")
+	dofile(LightingSystem_root.."Corona.lua")
+	dofile(LightingSystem_root.."Super player corona.lua")
 	
 	--Light object assignment (Definitions)
-	dofile(LightingSystem_root.."Definitions/Vanilla") --Vanilla SRB2
-	dofile(LightingSystem_root.."Definitions/Extras") --Lighting System additions
+	dofile(LightingSystem_root.."Definitions/Vanilla.lua") --Vanilla SRB2
+	dofile(LightingSystem_root.."Definitions/Extras.lua") --Lighting System additions
 
 	print("\131LightingSystem loaded sucessfully!")
 	LightingSystem_loaded = true
