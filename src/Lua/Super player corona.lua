f5ea9c5b8e13f383e7a9768d6586d6334b6e4bcd
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-20 17:03:43 -0400

    Make PlayerCoronaSpawn a PlayerThink instead

--- src/Lua/Super player corona.lua
+++ src/Lua/Super player corona.lua
@@ -5,31 +5,30 @@ NOTE: This is NOT a separated corona! is a dummy object for the player.
 then the corona spawns if this object is defined on the LightObjects[MT_] table
 */
 
-local function SuperCheck(mo)
-    local p = mo.player
-
-    if p.powers[pw_super] or (p.solchar and p.solchar.istransformed) then return true end
+local function SuperCheck(p)
+    if p.powers[pw_super] then return true end --Vanilla Super Form
+    if (p.solchar and p.solchar.istransformed) then return true end --Sol Forms
     return false
 end
 
-local function PlayerCoronaSpawn(mo)
-    if not (mo and mo.player and mo.valid and mo.health) then return end
+local function PlayerCoronaSpawn(p)
+    if not (p.mo and p and p.mo.valid and p.mo.health) then return end
 
-    if SuperCheck(mo) then
-        if not mo.supercorona then
-            mo.supercorona = P_SpawnMobjFromMobj(mo, 0,0,0, MT_PLAYERCORONA)
-            mo.supercorona.target = mo
-            mo.supercorona.scale = mo.scale
-            mo.supercorona.color = mo.color
+    if SuperCheck(p) then
+        if not p.mo.supercorona then
+            p.mo.supercorona = P_SpawnMobjFromMobj(p.mo, 0,0,0, MT_PLAYERCORONA)
+            p.mo.supercorona.target = p.mo
+            p.mo.supercorona.scale = p.mo.scale
+            p.mo.supercorona.color = p.mo.color
         end
-    elseif mo.supercorona then
-        mo.supercorona = nil
+    elseif p.mo.supercorona then
+        p.mo.supercorona = nil
     end
 end
 
 local function SuperCoronaThink(mo)
     local t = mo.target
-    if not (t and t.player and SuperCheck(t)) then P_KillMobj(mo) return end
+    if not (t and t.player and SuperCheck(t.player)) then P_KillMobj(mo) return end
     if mo.radius - t.radius then mo.radius = t.radius end
     if mo.height - t.height then mo.height = t.height end
     if mo.scale - t.scale then mo.scale = t.scale end
@@ -37,5 +36,5 @@ local function SuperCoronaThink(mo)
     Corona_Follow(mo, t)
 end
 
-addHook("MobjThinker", PlayerCoronaSpawn, MT_PLAYER)
+addHook("PlayerThink", PlayerCoronaSpawn)
 addHook("MobjThinker", SuperCoronaThink, MT_PLAYERCORONA)
