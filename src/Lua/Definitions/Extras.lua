6f7bbeff8e1e198a8236b3266ca63edd8a53e24c
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-20 02:45:54 -0400

    Adjust super player corona for centered_offset

--- src/Lua/Definitions/Extras.lua
+++ src/Lua/Definitions/Extras.lua
@@ -1,9 +1,10 @@
 --Lighting System exclusive additions
 
 LightObjects[MT_PLAYERCORONA] = { --Super player corona.lua
-    scale = FU/3*2,
+    scale = FU*4/5,
     alpha = FU/2,
     stayondeath = false,
     floorlight = true,
-    zoffset = -12
+    zoffset = 4,
+    centered_offset = true
 }
