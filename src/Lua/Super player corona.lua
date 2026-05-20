cb5e21e4d1302eab205d318f855bb8a13f56464c
GLide KS <84994993+GLideKS@users.noreply.github.com>
2026-05-20 16:58:01 -0400

    Super Player Corona supports Sol Forms (RushChars)

--- src/Lua/Super player corona.lua
+++ src/Lua/Super player corona.lua
@@ -5,10 +5,17 @@ NOTE: This is NOT a separated corona! is a dummy object for the player.
 then the corona spawns if this object is defined on the LightObjects[MT_] table
 */
 
+local function SuperCheck(mo)
+    local p = mo.player
+
+    if p.powers[pw_super] or (p.solchar and p.solchar.istransformed) then return true end
+    return false
+end
+
 local function PlayerCoronaSpawn(mo)
     if not (mo and mo.player and mo.valid and mo.health) then return end
 
-    if mo.player.powers[pw_super] then
+    if SuperCheck(mo) then
         if not mo.supercorona then
             mo.supercorona = P_SpawnMobjFromMobj(mo, 0,0,0, MT_PLAYERCORONA)
             mo.supercorona.target = mo
@@ -22,7 +29,7 @@ end
 
 local function SuperCoronaThink(mo)
     local t = mo.target
-    if not (t and t.player and t.player.powers[pw_super]) then P_KillMobj(mo) return end
+    if not (t and t.player and SuperCheck(t)) then P_KillMobj(mo) return end
     if mo.radius - t.radius then mo.radius = t.radius end
     if mo.height - t.height then mo.height = t.height end
     if mo.scale - t.scale then mo.scale = t.scale end
